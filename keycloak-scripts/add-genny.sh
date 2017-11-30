28c3f3eb7778df839f50b6ea9a33ca23a336f506
Adam Crow <acrow@Adams-MacBook-Pro.local>
2017-11-30 14:49:28 +1100

    Updated genny realm creation

--- keycloak-scripts/add-genny.sh
+++ keycloak-scripts/add-genny.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 ./add-customer.sh http://localhost:8080 ${KEYCLOAK_PASSWORD} genny ${KEYCLOAKURL} 056b73c1-7078-411d-80ec-87d41c55c3b4 Alyson Genny  password1 genny@genny.live genny+admin@genny.live
+cp -f realms/genny.json localhost.json 
+cp -f realms/genny.json 127.0.0.1.json
+
